9fdc202c4559805e0a523f416091121043055bb3
VladimirBushilov <ttkshibo@ya.ru>
2023-04-29 18:41:49 +0300

    Доработка

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,8 +6,14 @@
 
 Console.WriteLine("Введите пятизначное число: ");
 string? num = Console.ReadLine();
-if (num[0]==num[4] || num[1]==num[3])
+
+if (num!.Length == 5)
 {
+    if (num[0]==num[4] || num[1]==num[3])
+    {
     Console.WriteLine($"Yes"); 
+    }
+else 
+Console.WriteLine($"No");
 }
-else Console.WriteLine($"No");
+else System.Console.WriteLine("ОШИБКА! Введите пятизначное число: ");
